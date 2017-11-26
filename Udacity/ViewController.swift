d9a0a1b7cf3aa1e75beb0b859d9dd52335c635e9
sowmyay <syellapragada3@gatech.edu>
2017-11-26 13:24:57 -0500

    Initial Commit

---
+++ Udacity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udacity
+//
+//  Created by sowmya yellapragada on 11/26/17.
+//  Copyright © 2017 sowmya.yellapragada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
